90ff86db7bf9e0836ca638eb4d1905685530ef28
theadithyaaa <malsha.adithya95@gmail.com>
2024-11-23 20:49:01 +0530

    login ui created

--- src/app/pages/heropage/heropage.component.ts
+++ src/app/pages/heropage/heropage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heropage',
   standalone: true,
-  imports: [RouterLink, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, FormsModule],
   templateUrl: './heropage.component.html',
   styleUrl: './heropage.component.css'
 })
