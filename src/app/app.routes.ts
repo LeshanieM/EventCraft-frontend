1c24abc2178dd171f3288082047fc0a73d4d9be6
LeshanieM <Leshaniebiz@gmail.com>
2024-11-27 21:09:48 +0530

    UI created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { HeropageComponent } from './pages/heropage/heropage.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { BandComponent } from './pages/band/band.component';
+import { HallComponent } from './pages/hall/hall.component';
+import { CateringComponent } from './pages/catering/catering.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +20,25 @@ export const routes: Routes = [
     {
         path:"dashboard",
         component: DashboardComponent
+    },
+    {
+        path:"aboutus",
+        component: AboutusComponent
+    },
+    {
+        path:"band",
+        component: BandComponent
+    },
+    {
+        path:"hall",
+        component: HallComponent
+    },
+    {
+        path:"catering",
+        component: CateringComponent
+    },
+    {
+        path:"contact",
+        component: ContactComponent
     }
 ];
