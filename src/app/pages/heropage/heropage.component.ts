5af16f0c6af94c5da59b230b0f3dcdcaaa6d2c23
LeshanieM <Leshaniebiz@gmail.com>
2025-08-28 22:29:45 +0530

    heropage fixed

--- src/app/pages/heropage/heropage.component.ts
+++ src/app/pages/heropage/heropage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';  // <-- add this
 
 @Component({
   selector: 'app-heropage',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, FormsModule],
+  imports: [RouterLink, NavbarComponent, FormsModule, NgIf],
   templateUrl: './heropage.component.html',
   styleUrl: './heropage.component.css'
 })
